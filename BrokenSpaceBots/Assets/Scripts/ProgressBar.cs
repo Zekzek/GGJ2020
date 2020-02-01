58023da770198a6578302efcb7dbb700c22cad73
lonjporter <lonjporter@gmail.com>
2020-02-01 14:39:04 -0800

    Added Progress/Death Bar

---
+++ BrokenSpaceBots/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ProgressBar : MonoBehaviour
+{
+    public float maximum;
+    public float current;
+    public Image mask;
+    //public Image fill;
+    //public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+
+    }
+
+    void GetCurrentFill()
+    {
+        //float fillAmount = (float)current/(float)maximum;
+        //mask.fillAmount = fillAmount;
+
+        //fill.color = color;
+    }
+}
