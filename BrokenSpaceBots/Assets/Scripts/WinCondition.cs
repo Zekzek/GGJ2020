cd5768e58fe57aa8a334e66e0c3528c38fa9c130
lonjporter <lonjporter@gmail.com>
2020-02-02 13:16:15 -0800

    Added Win Condition

---
+++ BrokenSpaceBots/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+
+    Station[] station;
+
+    public GameObject winScreen;
+
+    // Start is called before the first frame update
+
+    public void Win()
+    {
+        winScreen.SetActive(true);
+
+    }
+    void Start()
+    {
+        station =  FindObjectsOfType(typeof(Station)) as Station[];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool allMaxHealth = true;
+        foreach(var s in station)
+        {
+            if(s.CurrentHealth<s.MaxHealth)
+            {
+                allMaxHealth=false;
+            }
+        }
+        if(allMaxHealth)
+        {
+            Win();
+        }
+    }
+
+}
